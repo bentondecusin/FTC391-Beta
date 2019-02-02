2d701d7389eca411e5a6e01a79d3fb0e806572bb
bentondecusin <2389507122@qq.com>
2019-02-02 11:52:04 -0500

    Finalize Driving Mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BDriving.java
@@ -17,33 +17,32 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class BDriving extends OpMode{
 
-    //motors
-    private Servo ser1 = null;
+    //set up motors
     private DcMotor left = null;
     private DcMotor right = null;
     private DcMotor lift = null;
+    private Servo gate = null;
 
-    //set up encoders
-    static final double COUNTS_PER_REV    = 1140 ;
-    static final double WHEEL_DIAMETER = 4 ; //in inches
-    static final double COUNTS_PER_INCH = COUNTS_PER_REV/(WHEEL_DIAMETER*Math.PI);
-    static final double COUNTS_Per_DEGREE = COUNTS_PER_REV/((130)/WHEEL_DIAMETER);
-
+    //set up speed
     private String speedStatus ;
-
     private double speed = .5 ;
-    private Boolean nutsTooMuch = false;
+
+
     @Override
     public void init() {
         left = hardwareMap.get(DcMotor.class, "mot0");
         right = hardwareMap.get(DcMotor.class, "mot1");
         lift = hardwareMap.get(DcMotor.class, "mot2");
+        gate = hardwareMap.get(Servo.class, "ser1");
 
         left.setDirection(DcMotor.Direction.FORWARD);
-        right.setDirection(DcMotor.Direction.REVERSE);
-        lift.setDirection(DcMotor.Direction.REVERSE);
+        left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right.setDirection(DcMotor.Direction.REVERSE);
+        right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        lift.setDirection(DcMotor.Direction.REVERSE);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         telemetry.addData("Status", "Initialized");
 
@@ -58,7 +57,7 @@ public class BDriving extends OpMode{
     public void start() {
 
     }
-    public ElapsedTime runTime = new ElapsedTime();
+
 
     public void checkKeys(){
         if (gamepad1.b == true){
@@ -69,25 +68,40 @@ public class BDriving extends OpMode{
             speed = .2;
             speedStatus = "Slow Speed";
         }
+
         if(gamepad1.a == true){
             speed = 0.5;
             speedStatus = "Medium Speed";
         }
-        if (gamepad1.left_bumper){
-            //   liftUp();
+
+        if(gamepad1.left_bumper == true){
+
+            gate.setPosition(.5); //open gate
         }
+        if(gamepad1.right_bumper == true){
+
+            gate.setPosition(1); //close gate. secure mineral
+
+        }
+
+
+
+
     }
+
 
 
     @Override
     public void loop() {
         checkKeys();
+
         left.setPower(speed*(gamepad1.left_stick_y-gamepad1.right_stick_x));
         right.setPower(speed*(gamepad1.left_stick_y+gamepad1.right_stick_x));
-        ser1.setPosition(gamepad1.left_trigger);
+        lift.setPower(gamepad1.left_trigger - gamepad1.right_trigger);
+
+        telemetry.addData("servo", gate.getPosition());
         telemetry.addData("Speed:",speed);
         telemetry.addData("Speed Status",speedStatus);
-      //  telemetry.addData("debug lift", lift.getCurrentPosition());
 
         telemetry.update();
     }

6d1794aa0ef5a9c3a4e4ba59e828be4ae57fd3ec
bentondecusin <2389507122@qq.com>
2019-02-07 15:21:11 -0500

    Driving Mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vermis.java
@@ -0,0 +1,105 @@
+/**
+ * Author: Benton Li '19
+ * Version: 2.0
+ *
+ * */
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="Vermis Driving")
+
+public class Vermis extends OpMode{
+
+    //set up motors
+    private DcMotor left = null;
+    private DcMotor right = null;
+    private DcMotor lift = null;
+    private Servo gate = null;
+
+    //set up speed
+    private String speedStatus ;
+    private double speed = .5 ;
+
+
+    @Override
+    public void init() {
+        left = hardwareMap.get(DcMotor.class, "mot0");
+        right = hardwareMap.get(DcMotor.class, "mot1");
+        lift = hardwareMap.get(DcMotor.class, "mot2");
+        gate = hardwareMap.get(Servo.class, "ser1");
+
+        left.setDirection(DcMotor.Direction.FORWARD);
+        left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        right.setDirection(DcMotor.Direction.REVERSE);
+        right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        lift.setDirection(DcMotor.Direction.REVERSE);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        telemetry.addData("Status", "Initialized");
+
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+
+    public void checkKeys(){
+        if (gamepad1.b == true){
+            speed = 1 ;
+            speedStatus = "Maximum Overdrive~";
+        }
+        if (gamepad1.right_stick_button){
+            speed = .2;
+            speedStatus = "Slow Speed";
+        }
+
+        if(gamepad1.a == true){
+            speed = 0.5;
+            speedStatus = "Medium Speed";
+        }
+
+        if(gamepad1.right_bumper == true){
+
+            gate.setPosition(0.8); //open gate
+        }
+        if(gamepad1.left_bumper == true){
+
+            gate.setPosition(.1); //close gate. secure mineral
+
+        }
+
+
+
+
+    }
+
+
+
+    @Override
+    public void loop() {
+        checkKeys();
+
+        left.setPower(speed*(gamepad1.left_stick_y-gamepad1.right_stick_x));
+        right.setPower(speed*(gamepad1.left_stick_y+gamepad1.right_stick_x));
+        lift.setPower(gamepad1.left_trigger - gamepad1.right_trigger);
+
+        telemetry.addData("Gate Status", gate.getPosition());
+        telemetry.addData("Speed:",speed);
+        telemetry.addData("Speed Status",speedStatus);
+
+        telemetry.update();
+    }
+}
